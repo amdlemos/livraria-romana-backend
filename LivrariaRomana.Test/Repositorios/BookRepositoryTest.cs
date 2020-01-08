c167c9f2a9f133ac200af59893ea141d201997df
alan <amdlemos@gmail.com>
2020-01-08 19:58:46 -0200

    Implementado primeiro teste

---
+++ LivrariaRomana.Test/Repositorios/BookRepositoryTest.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using LivrariaRomana.Infrastructure.Repositories.Domain;
+using LivrariaRomana.Infrastructure.DBConfiguration;
+
+namespace LivrariaRomana.Test.Repositorios
+{
+    public class BookRepositoryTest
+    {
+        private readonly BookRepository _bookRepository;
+        private readonly DataBaseContext _dbContext;
+
+        public BookRepositoryTest()
+        {
+            _dbContext = new DataBaseContext();
+            _bookRepository = new BookRepository(_dbContext);
+        }        
+
+        [Fact]
+        public void LoadBook()
+        {
+            var result = _bookRepository.GetAllIncludingBooksAsync();
+
+            Assert.NotNull(result);
+        }
+    }
+}
