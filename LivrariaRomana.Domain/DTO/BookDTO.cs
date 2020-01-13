e38a59782aa70a57c590493110c146505ea2265d
alan <amdlemos@gmail.com>
2020-01-12 22:48:25 -0200

    Adicionado DTOs

---
+++ LivrariaRomana.Domain/DTO/BookDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LivrariaRomana.Domain.DTO
+{
+    public class BookDTO
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string OriginalTitle { get; set; }
+        public string Author { get; set; }
+        public string publishingCompany { get; set; }
+        public string publicationYear { get; set; }
+        public string isbn { get; set; }
+        public int amount { get; set; }
+    }
+}
