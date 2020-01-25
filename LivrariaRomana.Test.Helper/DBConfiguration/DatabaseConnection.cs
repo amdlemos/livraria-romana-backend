c29bd40a933f7213090c16305abe31a34f5c631d
alan <amdlemos@gmail.com>
2020-01-25 16:02:53 -0200

    Alteração nomenclaturas

---
+++ LivrariaRomana.Test.Helper/DBConfiguration/DatabaseConnection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace LivrariaRomana.Test.Helper
+{
+    public class DatabaseConnection
+    {
+        public static IConfiguration ConnectionConfiguration
+        {
+            get
+            {
+                var path = Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.ToString()).FullName.ToString();                
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                    .SetBasePath(path)
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+                return configuration;// Options.Create(Configuration.GetSection("ConnectionStrings").Get<DataOptionFactory>());
+            }
+        }
+    }
+}
---
+++ LivrariaRomana.Test.Helper/DataBuilder/UserBuilder.cs
@@ -0,0 +1,84 @@
+﻿using LivrariaRomana.Domain.Entities;
+using System.Collections.Generic;
+
+namespace LivrariaRomana.Test.Helper
+{
+    public class UserBuilder
+    {
+        private User user;
+        private List<User> userList;
+
+        public User CreateUser()
+        {
+            user = new User("user", "123", "user@builder.com");            
+            return user;
+        }
+
+        public User CreateUserWithEmptyUsername()
+        {
+            user = new User("", "123", "user@builder.com", "");
+            return user;
+        }
+
+        public User CreateUserWithNullUsername()
+        {
+            user = new User(null, "123", "user@builder.com", "");
+            return user;
+        }
+
+
+        public User CreateUserWithNullPassword()
+        {
+            user = new User("User from Builder", null, "user@builder.com");
+            return user;
+        }
+
+        public User CreateUserWithEmptyPassword()
+        {
+            user = new User("User from Builder", "", "user@builder.com");
+            return user;
+        }
+
+        public User CreateUserWithNullEmail()
+        {
+            user = new User("User from Builder","123" , null);
+            return user;
+        }
+
+        public User CreateUserWithEmptyEmail()
+        {
+            user = new User("User from Builder", "123", "");
+            return user;
+        }
+
+        public User CreateUserWithInvalidEmail()
+        {
+            user = new User("User from Builder", "123", "aaaa");
+            return user;
+        }
+
+        public User CreateUserWithNonexistentId()
+        {
+            user = new User("User from Builder", "123", "aaaa@gmail.com", "", 9999999);
+            return user;
+        }
+
+
+        public User CreateUserWithId()
+        {
+            user = new User("User from Builder", "123", "aaaa@gmail.com", "", 1);
+            return user;
+        }
+
+        public List<User> CreateUserList(int amount)
+        {
+            userList = new List<User>();
+            for (int i = 0; i < amount; i++)
+            {
+                userList.Add(CreateUser());
+            }
+
+            return userList;
+        }
+    }
+}
